c4b9b7d4a7c53b6458d2913e49c9cdf31cbd892e
Vile <111662603+vile@users.noreply.github.com>
2024-01-02 15:19:46 -0500

    fix: add command_prefix argument to bot init

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 
+
 class AttendanceBot(commands.Bot):
     def __init__(self) -> None:
         client_intents = discord.Intents.all()
@@ -10,22 +11,25 @@ class AttendanceBot(commands.Bot):
         client_intents.message_content = False
 
         super().__init__(
+            command_prefix=commands.when_mentioned,
             intents=client_intents,
         )
 
         self.remove_command("help")
 
         # Load cogs
-        for filename in os.listdir('./cogs'):
-            if filename.endswith('.py'):
-                self.load_extension(f'cogs.{filename[:-3]}')
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                self.load_extension(f"cogs.{filename[:-3]}")
 
     def run(self) -> None:
         super().run()
 
+
 def main() -> None:
     client = AttendanceBot()
     client.run()
+
 
 if __name__ == "__main__":
     main()
