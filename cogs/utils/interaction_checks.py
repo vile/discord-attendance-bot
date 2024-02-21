59abac441de1cf1aa843c9b860090ecfa1b6d9a1
Vile <111662603+vile@users.noreply.github.com>
2024-02-20 23:55:08 -0500

    refactor: remove unused function param

--- cogs/utils/interaction_checks.py
+++ cogs/utils/interaction_checks.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import cogs.utils.shelve_utils as shelve_utils
 
 
-async def user_is_instructor(client: commands.Bot, interaction: Interaction) -> bool:
+async def user_is_instructor(interaction: Interaction) -> bool:
     if interaction.user.id in shelve_utils.get_instructors():
         return True
     return False
@@ -17,7 +17,7 @@ async def user_is_owner(client: commands.Bot, interaction: Interaction) -> bool:
 async def user_is_instructor_or_owner(
     client: commands.Bot, interaction: Interaction
 ) -> bool:
-    if await user_is_instructor(client, interaction):
+    if await user_is_instructor(interaction):
         return True
     if await user_is_owner(client, interaction):
         return True
